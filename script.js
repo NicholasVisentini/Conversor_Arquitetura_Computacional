75268f4e8caf733544be9962e35344241c2e1501
NicholasVisentini <nicholas.santos@sptech.school>
2026-03-24 23:33:53 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+function converter() {
+    const decimal = Number(document.getElementById("demo1").value);
+
+    if (isNaN(decimal)) {
+        alert("Digite um número válido!");
+        return;
+    }
+
+    document.getElementById("demo2").innerText = decimal.toString(8);
+    document.getElementById("demo3").innerText = decimal.toString(16).toUpperCase();
+    document.getElementById("demo4").innerText = decimal.toString(2);
+}
+
+const selectTema = document.querySelector(".select_temas");
+selectTema.addEventListener("change", function() {
+    document.body.className = "";
+    switch(this.value) {
+        case "matrix": document.body.classList.add("tema-matrix"); break;
+        case "claro": document.body.classList.add("tema-claro"); break;
+        case "escuro": document.body.classList.add("tema-escuro"); break;
+    }
+});
